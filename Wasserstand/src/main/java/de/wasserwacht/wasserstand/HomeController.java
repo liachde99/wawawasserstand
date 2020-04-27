52f35d4729f0c58c8cdfb3a64bb7d0eff4fb18f8
Julian <Julian@Julian-PC.fritz.box>
2020-04-27 18:13:37 +0200

    2nd Push

---
+++ Wasserstand/src/main/java/de/wasserwacht/wasserstand/HomeController.java
@@ -0,0 +1,15 @@
+package de.wasserwacht.wasserstand;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public ModelAndView home() {
+		ModelAndView mv = new ModelAndView("index.html");
+		return mv;
+	}
+}
