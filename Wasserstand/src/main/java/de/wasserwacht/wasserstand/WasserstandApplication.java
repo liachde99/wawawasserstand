6d07f8536da2a592616d14420b57bb4fb12f0f6d
Julian <Julian@Julian-PC.fritz.box>
2020-04-27 18:06:52 +0200

    First Commit

---
+++ Wasserstand/src/main/java/de/wasserwacht/wasserstand/WasserstandApplication.java
@@ -0,0 +1,13 @@
+package de.wasserwacht.wasserstand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasserstandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasserstandApplication.class, args);
+	}
+
+}
---
+++ Wasserstand/src/test/java/de/wasserwacht/wasserstand/WasserstandApplicationTests.java
@@ -0,0 +1,13 @@
+package de.wasserwacht.wasserstand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasserstandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
