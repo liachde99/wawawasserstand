1e0d96fa83a69f0577e49c0398675d9eb9aa22cb
Julian <Julian@Julian-PC.fritz.box>
2020-04-27 18:29:54 +0200

    thymeleaf test

--- Wasserstand/src/main/java/de/wasserwacht/wasserstand/HomeController.java
+++ Wasserstand/src/main/java/de/wasserwacht/wasserstand/HomeController.java
@@ -2,14 +2,29 @@ package de.wasserwacht.wasserstand;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	
+	private int wasserstand;
+	
 	@GetMapping("/")
+	public int test() {
+		return 1;
+	}
+	
+	@GetMapping("/request")
 	public ModelAndView home() {
 		ModelAndView mv = new ModelAndView("index.html");
+		
+		mv.addObject("wasserstand", this.wasserstand);
 		return mv;
+	}
+	
+	@GetMapping("/send/{stand}")
+	public void sendwasserstand(@PathVariable("stand") int stand) {
+		stand = wasserstand;
 	}
 }
