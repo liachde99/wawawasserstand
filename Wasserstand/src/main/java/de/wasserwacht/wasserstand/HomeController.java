ad84d5290abde5a07cf96a9259428129ca65d4d0
Julian <Julian@Julian-PC.fritz.box>
2020-04-28 22:18:31 +0200

    reset

--- Wasserstand/src/main/java/de/wasserwacht/wasserstand/HomeController.java
+++ Wasserstand/src/main/java/de/wasserwacht/wasserstand/HomeController.java
@@ -20,40 +20,43 @@ public class HomeController {
 	private WasserstandService wasserstandService;
 	
 	@GetMapping("/")
-	public ModelAndView home() {
-		ModelAndView mv = new ModelAndView("index.html");
-		
-		String date;
-		int stand;
-		Wasserstand last = wasserstandService.findLast();
+	public ModelAndView index() {
+		return new ModelAndView("index2.html");
+	}
+//	public ModelAndView home() {
+//		ModelAndView mv = new ModelAndView("index.html");
+//		
+//		String date;
+//		int stand;
+//		Wasserstand last = wasserstandService.findLast();
 //		List<Wasserstand> today = wasserstandService.findByDay(new SimpleDateFormat("dd").format(new Date()));
 //		List<Wasserstand> month = wasserstandService.findByMonth(new SimpleDateFormat("MM").format(new Date()));
 //		List<Wasserstand> year = wasserstandService.findByYear(new SimpleDateFormat("YYYY").format(new Date()));
-		
-		if(last!=null) {
-			date = last.getDay() + '.' + last.getMonth() + ' ' + last.getHour() + ':' + last.getMin();
-			stand = last.getStand();
-		}else {
-			date = "none";
-			stand = 0;
-		}
-		
-		mv.addObject("lasttimestamp", date);
-		mv.addObject("wasserstand", stand);
-		
+//		
+//		if(last!=null) {
+//			date = last.getDay() + '.' + last.getMonth() + ' ' + last.getHour() + ':' + last.getMin();
+//			stand = last.getStand();
+//		}else {
+//			date = "none";
+//			stand = 0;
+//		}
+//		
+//		mv.addObject("lasttimestamp", date);
+//		mv.addObject("wasserstand", stand);
+//		
 //		mv.addObject("tag", today);
 //		mv.addObject("monat", month);
 //		mv.addObject("jahr", year);
-		return mv;
-	}
-	
-	
-	
-	@GetMapping("/{passwort}/{stand}")
-	public String sendwasserstand(@PathVariable("passwort") String passwort,@PathVariable("stand") int stand) {
-		if(passwort.equalsIgnoreCase("gxcxWUxezdAgrhZz2EZH")) {
-			wasserstandService.save(new Wasserstand(stand));
-		}
-		return "";
-	}
+//		return mv;
+//	}
+//	
+//	
+//	
+//	@GetMapping("/{passwort}/{stand}")
+//	public String sendwasserstand(@PathVariable("passwort") String passwort,@PathVariable("stand") int stand) {
+//		if(passwort.equalsIgnoreCase("gxcxWUxezdAgrhZz2EZH")) {
+//			wasserstandService.save(new Wasserstand(stand));
+//		}
+//		return "";
+//	}
 }
