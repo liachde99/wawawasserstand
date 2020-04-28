206ac8105b0b0779ac5e5bb67ee85d3f6d3886ed
Julian <Julian@Julian-PC.fritz.box>
2020-04-28 16:41:55 +0200

    aufräumen

--- Wasserstand/src/main/java/de/wasserwacht/wasserstand/HomeController.java
+++ Wasserstand/src/main/java/de/wasserwacht/wasserstand/HomeController.java
@@ -1,8 +1,5 @@
 package de.wasserwacht.wasserstand;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,8 +14,6 @@ public class HomeController {
 	
 	@Autowired
 	private WasserstandService wasserstandService;
-	
-	private String time;
 	
 	@GetMapping("/")
 	public ModelAndView home() {
