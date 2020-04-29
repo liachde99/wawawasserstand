46c8e90bbb5ee20c4fd435acc146d8f95888d3ef
Julian <julian.lichtenauer1999@gmail.com>
2020-04-29 09:50:33 +0200

    spelling

--- Wasserstand/src/main/java/de/wasserwacht/wasserstand/PostgresConfig/DatabaseConfig.java
+++ Wasserstand/src/main/java/de/wasserwacht/wasserstand/PostgresConfig/DatabaseConfig.java
@@ -12,7 +12,7 @@ import com.zaxxer.hikari.HikariDataSource;
 @Configuration
 public class DatabaseConfig {
 	
-	@Value("${spring.datasource.url]")
+	@Value("${spring.datasource.url}")
 	private String dbUrl;
 	
 	@Bean
