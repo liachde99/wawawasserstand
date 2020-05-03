e2414f0db587c93510c352df4a99fcf4b00a545e
Julian <julian.lichtenauer1999@gmail.com>
2020-05-03 14:04:25 +0200

    schedule

--- Wasserstand/src/main/java/de/wasserwacht/wasserstand/WasserstandApplication.java
+++ Wasserstand/src/main/java/de/wasserwacht/wasserstand/WasserstandApplication.java
@@ -2,8 +2,10 @@ package de.wasserwacht.wasserstand;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class WasserstandApplication {
 
 	public static void main(String[] args) {
