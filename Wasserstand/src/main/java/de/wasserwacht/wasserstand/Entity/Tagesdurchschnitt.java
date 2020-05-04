936f12d092d315cd00031fd0784908fefd8fd28b
Julian <julian.lichtenauer1999@gmail.com>
2020-05-04 14:10:53 +0200

    identity

--- Wasserstand/src/main/java/de/wasserwacht/wasserstand/Entity/Tagesdurchschnitt.java
+++ Wasserstand/src/main/java/de/wasserwacht/wasserstand/Entity/Tagesdurchschnitt.java
@@ -17,7 +17,7 @@ import javax.validation.constraints.NotNull;
 public class Tagesdurchschnitt {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id", unique = true, nullable = false)
 	private Long id;
 	
