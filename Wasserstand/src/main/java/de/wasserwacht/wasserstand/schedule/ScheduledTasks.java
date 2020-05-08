55ace0b88b002e4cef07e218432ae58bd2e8b68c
Julian <julian.lichtenauer1999@gmail.com>
2020-05-09 00:12:55 +0200

    awdawd

--- Wasserstand/src/main/java/de/wasserwacht/wasserstand/schedule/ScheduledTasks.java
+++ Wasserstand/src/main/java/de/wasserwacht/wasserstand/schedule/ScheduledTasks.java
@@ -41,14 +41,6 @@ public class ScheduledTasks {
 	@Autowired
 	private LastsevendaysService lsdService;
 	
-	@Scheduled(fixedDelay = 1000)
-	public void dare() {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-	      Date now = new Date();
-	      String strDate = sdf.format(now);
-	      System.out.println("Fixed Delay scheduler:: " + strDate);
-	}
-	
 	@Scheduled(cron = "0 0 0 * * ?")
 	public void daily() {
 		tagesdurchschnitt();
