d651cde3df947eaad4fb80d5a141190ccf721ff5
Julian <julian.lichtenauer1999@gmail.com>
2020-05-11 08:52:50 +0200

    ztgt

--- Wasserstand/src/main/java/de/wasserwacht/wasserstand/schedule/ScheduledTasks.java
+++ Wasserstand/src/main/java/de/wasserwacht/wasserstand/schedule/ScheduledTasks.java
@@ -1,16 +1,12 @@
 package de.wasserwacht.wasserstand.schedule;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.IsoFields;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -42,49 +38,6 @@ public class ScheduledTasks {
 	
 	@Autowired
 	private LastsevendaysService lsdService;
-	
-	
-	
-	private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
-
-	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-	
-	@Scheduled(fixedDelay = 3000)
-	public void test() {
-		log.info("The time is now {}", dateFormat.format(new Date()));
-		int durchschnitt = 0;
-		double tempdurchschnitt = 0;
-		int counter = 0;
-		
-		date =  LocalDateTime.now(ZoneId.of("CET")).minus(1, ChronoUnit.DAYS);
-		staende = service.findByDayAndMonthAndYear(9, 5, 2020);
-		
-		if(staende.size()!=0) {
-			for (Wasserstand wasserstand : staende) {
-				durchschnitt += wasserstand.getWasserstand();
-				tempdurchschnitt += wasserstand.getTemperatur();
-				counter++;
-			}
-			
-			if(tdservice.findByDayAndMonthAndYear(9, 5, 2020)==null) {
-				tdservice.save(new Tagesdurchschnitt((durchschnitt/counter),(tempdurchschnitt/counter),9,5,2020,19));
-			}
-		}
-		
-		staende = service.findByDayAndMonthAndYear(10, 5, 2020);
-		
-		if(staende.size()!=0) {
-			for (Wasserstand wasserstand : staende) {
-				durchschnitt += wasserstand.getWasserstand();
-				tempdurchschnitt += wasserstand.getTemperatur();
-				counter++;
-			}
-			
-			if(tdservice.findByDayAndMonthAndYear(date.getDayOfMonth(), date.getMonthValue(), date.getYear())==null) {
-				tdservice.save(new Tagesdurchschnitt((durchschnitt/counter),(tempdurchschnitt/counter),10,5,2020,19));
-			}
-		}
-	}
 	
 	
 	@Scheduled(cron = "0 0 2 * * ?")
