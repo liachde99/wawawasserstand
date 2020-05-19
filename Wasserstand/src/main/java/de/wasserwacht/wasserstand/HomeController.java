829d8a36467110e882c19e227511c020aaf4389d
Julian <julian.lichtenauer1999@gmail.com>
2020-05-19 10:48:15 +0200

    awdawd

--- Wasserstand/src/main/java/de/wasserwacht/wasserstand/HomeController.java
+++ Wasserstand/src/main/java/de/wasserwacht/wasserstand/HomeController.java
@@ -146,9 +146,8 @@ public class HomeController {
 	
 	public boolean checkDbUpdateDaily() {
 		LocalDateTime date = LocalDateTime.now(ZoneId.of("CET"));
-		date.minus(1, ChronoUnit.DAYS);
 		
-		if(tagesdurchschnittService.findByDayAndMonthAndYear(date.getDayOfMonth(), date.getMonthValue(), date.getYear())!=null) {
+		if(tagesdurchschnittService.findByDayAndMonthAndYear(date.getDayOfMonth()-1, date.getMonthValue(), date.getYear())!=null) {
 			return false;
 		}
 		return true;
